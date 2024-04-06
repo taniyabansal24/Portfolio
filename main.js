8b9191a88376cae8dfbb3dfa5aace23a82b76708
Taniya Bansal <149322176+taniyabansal24@users.noreply.github.com>
2024-04-06 06:30:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+
 var typed = new Typed(".text",{
     strings: ["Frontend Developer", "Backend Developer", "Web Developer"],
     typeSpeed: 100,
